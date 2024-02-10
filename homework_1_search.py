3cddd9c3282b9ca8a7989f1eb7f954b72204fc98
DiegoOzuna <ozunad2002@gmail.com>
2024-02-10 14:22:26 -0800

    Implemented A* Search. Same UCS structure; difference: Cost includes heuristic.

--- homework_1_search.py
+++ homework_1_search.py
@@ -129,28 +129,28 @@ class SearchAlgorithms:
             Visited.append(child)      #Mark the child as visited...
             pQueue.put((weights[(node, child)] + currentTuple[0], child, currentTuple[2] + [child]))    #And put the child, along with its weight + previous cost, and updated path, into our priority queue .
 
+    return "Nothing Found" #backup if no route was found.
+  
+
+  def AStar(self, start, goal, graph, weights, heuristic):
+    pQueue = PriorityQueue()
+    pQueue.put((0, start, [start]))   #We will utilize the same 3-tuple structure utilized in UCS. cost, start, and path.
+    Visited = [start]                 #we start from start therefore it is automatically visited
+
+    while pQueue:                     #structure of UCS will be replicated, only difference will be the cost that is calculated.
+       currentTuple = pQueue.get()
+       node = currentTuple[1]
+       children = graph.get(node, [])
+
+       if node == goal:    #again, we note that if the current node (which was just dequeued from the priority queue) is our goal, then it is our lowest possible cost route to the goal
+          return "Returned Solution: ", currentTuple[2], "Expanded Cities: ", Visited
+       
+       for child in children:
+          if child not in Visited:
+             Visited.append(child)
+             pQueue.put((weights[(node, child)] + currentTuple[0] + heuristic[(child)], child, currentTuple[2] + [child]))    #difference here is that we are also adding on to the cost, the heuristic
 
     return "Nothing Found" #backup if no route was found.
-  def AStar(self, start, goal, graph, weights, heuristic):
-    """Search the node that has the lowest combined cost and heuristic first.
-    Important things to remember
-    1 - Use PriorityQueue with .put() and .get() functions
-    2 - In addition to putting the start or current node in the queue, and the
-    g(n), also put the combined cost (i.e., g(n) + h(n))
-    using weights and heuristic data structure
-    3 - When you're expanding the neighbor of the current you're standing at,
-    get its g(neighbor) by weights[(node, neighbor)]
-    4 - Calling weights[(node, neighbor)] may throw KeyError exception which is
-    due to the fact that the weights data structure
-    only has one directional weights. In the class, we mentioned that there
-    is a path from Arad to Sibiu and back. If the
-    exception occurs, you will need to get the weight of the nodes in
-    reverse direction (weights[(neighbor, node)])
-    """
-    "*** YOUR CODE HERE ***"
-    # You can delete the line below once you have implemented your solution
-    #above
-    return {"Returned solution: [], Expanded cities: []"}
 
 # Call to create the object of the above class
 search = SearchAlgorithms()
